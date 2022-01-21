b4989ef1d4025d9fef1336b4fa940263386d5a71
John Leskas <john.leskas@gmail.com>
2022-01-21 16:20:16 +0200

    :sparkles: Basic markup for main page

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1 className="max-w-xl mx-auto text-3xl font-bold underline">
-      Form Builder App
-    </h1>
+    <div className="px-4 py-4 sm:px-0">
+      <div className="border-4 border-dashed border-gray-200 rounded-lg h-96" />
+      <h1 className="max-w-xl mx-auto text-3xl font-bold underline"></h1>
+    </div>
   );
 }
